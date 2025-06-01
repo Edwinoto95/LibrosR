c0f47c29e5af1469c5eb8f33fc22118312324373
Tu Nombre <tu-correo@ejemplo.com>
2025-05-31 23:13:25 -0500

    pruebaasssssssssssssda

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-
-# Salir si ocurre algún error
 set -o errexit
 
 echo "Actualizando pip..."
 pip install --upgrade pip
+
 echo "Instalando dependencias..."
 pip install -r requirements.txt
+
 echo "Ejecutando migraciones..."
 python manage.py migrate --noinput
+
 echo "Recolectando archivos estáticos..."
 python manage.py collectstatic --noinput
+
 echo "Build completado correctamente."
